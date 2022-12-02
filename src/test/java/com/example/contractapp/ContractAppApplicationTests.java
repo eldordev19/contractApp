f496e7b8164f4683484bc1f4446fd86cf7164147
eldordev19 <eldordev19@gmail.com>
2022-12-02 14:14:18 +0500

    first commit

---
+++ src/test/java/com/example/contractapp/ContractAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contractapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
